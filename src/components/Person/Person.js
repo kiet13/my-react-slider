d6a4b9b1c64c24d016ef1f2fddea9026a0ef5319
Nguyen Thanh Kiet <kiet13.itus@gmail.com>
2021-10-13 23:51:19 +0700

    Finish auto sliding

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,12 +2,22 @@ import React from 'react'
 import styles from './Person.module.scss'
 
 export default function Person(props) {
+  const typeClasses = [styles.Person];
+  if (props.type === 'prev') {
+    typeClasses.push(styles.Prev)
+  } else if (props.type === 'curr') {
+    typeClasses.push(styles.Curr)
+  } else if (props.type === 'next') {
+    typeClasses.push(styles.Next)
+  }
+
   return (
-    <div className={[styles.Person, "mt-4"].join(' ')}>
+    <div className={typeClasses.join(' ')}>
       <img className={styles.PersonImage} src={props.image} alt="avatar" />
       <h2 className={styles.PersonName}>{props.name}</h2>
       <h3 className={styles.PersonTitle}>{props.title}</h3>
       <p className={styles.PersonQuote}>{props.quote}</p>
+    
     </div>
   )
 }
